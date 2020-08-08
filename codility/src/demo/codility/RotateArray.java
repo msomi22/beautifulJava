da15ed9bb27f168accac12dad7c240c34b2d9108
Peter Mwenda Njeru <pmnjeru@kcbgroup.com>
2020-08-08 18:46:57 +0300

    add code lever comment

--- codility/src/demo/codility/RotateArray.java
+++ codility/src/demo/codility/RotateArray.java
@@ -1,20 +1,29 @@
 /**
- * * # Original Array
-	 *number: 10 20 30 40 50 60
-	 *index_: 0  1  2  3  4  5
-	 *
-	 *# First iteration 
-	 *number: 60 10 20 30 40 50
-	 *index_: 0  1  2  3  4  5 
-	 *
-	 *# Second iteration
-	 *number: 50 60 10 20 30 40
-	 *index_: 0  1  2  3  4  5 
-	 *
-	 *# Third iteration
-	 *number: 40 50 60 10 20 30
-	 *index_: 0  1  2  3  4  5 
-	 *
+ * You are given an array of integers as shown below,
+ * 
+ * int[] arr = 10 20 30 40 50 60
+ * index_: 0  1  2  3  4  5
+ * 
+ * You are required to rotate the array n times such that the last element
+ * will be at the first index, the element in the first index will shift to second index
+ * and so on
+ * 
+ * Below is a simple representation of how the new array will look like give n = 3
+ * 
+ * # First iteration 
+ * number: 60 10 20 30 40 50
+ * index_: 0  1  2  3  4  5 
+ * 
+ * # Second iteration
+ * number: 50 60 10 20 30 40
+ * index_: 0  1  2  3  4  5 
+ * 
+ * # Third iteration
+ * number: 40 50 60 10 20 30
+ * index_: 0  1  2  3  4  5 
+ * 
+ * The final array will be 40 50 60 10 20 30
+ * 
  */
 package demo.codility;
 
